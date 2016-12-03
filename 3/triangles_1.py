b6c773ee40782d2489c373764b93b8d03c8d25d8
k0ngui <kongui@live.com>
2016-12-03 14:49:41 +0100

    Removed "with" statement from day 3 solutions.

--- 3/triangles_1.py
+++ 3/triangles_1.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 
 def possible_triangles(path): 
-    triangles = []
-    with open(path, 'r') as f:
-        triangles = [sorted([int(x) for x in l.rstrip().split()]) for l in f.readlines()]
+    triangles = [sorted(map(int, line.split())) for line in open(path)]
     return sum([t[0] + t[1] > t[2] for t in triangles])
 
 if __name__ == '__main__':
--- 3/triangles_2.py
+++ 3/triangles_2.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 
 def possible_triangles(path): 
-    triangles = []
-    with open(path, 'r') as f:
-        triangles = [[int(x) for x in l.rstrip().split()] for l in f.readlines()]
+    triangles = [map(int, line.split()) for line in open(path)]
     triangles = [sorted(c[i:i + 3]) for c in zip(*triangles) for i in range(0, len(c), 3)]
     return sum([t[0] + t[1] > t[2] for t in triangles])
 
