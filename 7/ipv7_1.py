9a7d565d2ebbaa0cde7cc55525b58746dd0a7e57
k0ngui <kongui@live.com>
2016-12-07 13:10:55 +0100

    First part of Day 7.

---
+++ 7/ipv7_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import re
+
+def check_tls(path): 
+    abba_bkt = re.compile(r'\[\w*(.)(?!\1)(.)\2\1\w*\]')
+    abba = re.compile(r'(.)(?!\1)(.)\2\1') 
+    return sum([re.search(abba_bkt, a) == None and 
+                re.search(abba, a) != None for a in open(path)])
+
+if __name__ == '__main__':
+    print('TLS support in', check_tls('input.txt'), 'addresses.')
+
